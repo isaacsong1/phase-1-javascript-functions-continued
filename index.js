bfdbffbaafaa5c901932256711844e7ae9bf0396
Isaac Song <isaacsongs10@gmail.com>
2023-09-11 15:01:37 -0700

    Add function 2

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function saturdayFun(activity = 'roller-skate') {
     return `This Saturday, I want to ${activity}!`;
 }
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
